398ea48fd7497161e4ba8afa7ab1e21b69fdf47f
Julius Suominen <julius.suominen@gmail.com>
2017-07-30 17:01:04 +0200

    added header link logged in as

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <div class="text-center">
       <h1>404 Not Found</h1>
-      <p>You may be lost. Go back <a routerLink="/">home?</a>.</p>
+      <p>You may be lost. Go back <a routerLink="/">home</a>?</p>
     </div>
   `,
   styleUrls: ['./not-found.component.css']
