8d599743ea021432c1a320a30cdfd760b95fb6e3
Julius Suominen <julius.suominen@gmail.com>
2017-08-02 13:11:49 +0200

    config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { FrontpageComponent } from './components/frontpage/frontpage.component';
+import { DashboardColumnsComponent } from './components/dashboard-columns/dashboard-columns.component';
+import { DashboardLeadcardComponent } from './components/dashboard-leadcard/dashboard-leadcard.component';
+import { DashboardNewleadComponent } from './components/dashboard-newlead/dashboard-newlead.component';
+import { LeadComponent } from './components/lead/lead.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LeadEditComponent } from './components/lead-edit/lead-edit.component';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -12,18 +18,13 @@ import { HttpModule } from '@angular/http';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 import { ClarityModule } from 'clarity-angular';
 
-import { appRouting } from './app.routing';
-
 import { SessionService } from './services/session.service';
 import { SignupService } from './services/signup.service';
 import { LeadService } from './services/lead.service';
 import { ExternalService } from './services/external.service';
-import { DashboardColumnsComponent } from './components/dashboard-columns/dashboard-columns.component';
-import { DashboardLeadcardComponent } from './components/dashboard-leadcard/dashboard-leadcard.component';
-import { DashboardNewleadComponent } from './components/dashboard-newlead/dashboard-newlead.component';
-import { LeadComponent } from './components/lead/lead.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LeadEditComponent } from './components/lead-edit/lead-edit.component';
+
+import { appRouting } from './app.routing';
+
 import { LeadstatusPipe } from './pipes/leadstatus.pipe';
 
 @NgModule({
