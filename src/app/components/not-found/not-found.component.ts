8e1c5c927ab9249767bea1b6e621a5ca3ab438c4
Julius Suominen <julius.suominen@gmail.com>
2017-08-02 12:09:14 +0200

    added not found sad face

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <div class="text-center">
       <h1>404 Not Found</h1>
+      <clr-icon class="sad-face" shape="sad-face"></clr-icon>
       <p>You may be lost. Go back <a routerLink="/">home</a>?</p>
     </div>
   `,
