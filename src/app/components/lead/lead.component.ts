6680938f6782506a2c6b4766782e86fa8ceaaeea
Julius Suominen <julius.suominen@gmail.com>
2017-08-06 16:32:40 +0200

    file upload button

--- src/app/components/lead/lead.component.ts
+++ src/app/components/lead/lead.component.ts
@@ -154,4 +154,8 @@ export class LeadComponent implements OnInit {
   toggleEmailInfoModal() {
     this.emailInfoModalActive = !this.emailInfoModalActive;
   }
+
+  addAttachment() {
+    console.log('works!');
+  }
 }
